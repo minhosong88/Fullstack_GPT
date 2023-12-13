ae05b6af372cdad70913d135588e8dc4d5b825f6
minhosong88 <hominsong@naver.com>
2023-12-12 21:01:25 -0800

    Update DocGPT
    Add memory module to the app

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, List, Optional, Union
 from uuid import UUID
 from langchain.schema.output import ChatGenerationChunk, GenerationChunk
 import streamlit as st
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.storage import LocalFileStore
@@ -11,15 +11,15 @@ from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.vectorstores.faiss import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks.base import BaseCallbackHandler
+from langchain.memory import ConversationSummaryBufferMemory
 
 st.set_page_config(
     page_title="FullStackGPT DocumentGPT",
     page_icon="📄",
 )
 
+
 # Define a class for callback functions
-
-
 class ChatCallbackHandler(BaseCallbackHandler):
     # Initialize a message variable
     message =""
@@ -31,7 +31,6 @@ class ChatCallbackHandler(BaseCallbackHandler):
     def on_llm_end(self, *args, **kwargs):
         save_message(self.message, "ai")
 
-
     # Each new token generated, the function is called
     def on_llm_new_token(self, token, *args, **kwargs):
         # append each token to the message variable
@@ -39,7 +38,7 @@ class ChatCallbackHandler(BaseCallbackHandler):
         # each message appened will be shown to the message box
         self.message_box.markdown(self.message)
 
-# Define LLM
+# Create an LLM
 llm = ChatOpenAI(
     temperature=0.1,
     streaming=True,
@@ -48,6 +47,13 @@ llm = ChatOpenAI(
     ]
 )
 
+# Create a memory
+memory = ConversationSummaryBufferMemory(
+    llm=llm,
+    max_token_limit=120,
+    memory_key="chat_history",
+    return_messages=True,
+)
 
 # a function that return an embedded retriever
 # Use 'cache_data' decorator not to run the function again if the file is the same as earlier
@@ -79,11 +85,16 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+# Save the message and memory to the session_state
 def save_message(message, role):
-            st.session_state["messages"].append(
-            {"message": message, "role": role}
-            )
-
+    st.session_state["messages"].append(
+        {"message": message, "role": role}
+    )
+def save_memory(input, output):
+    st.session_state["chat_history"].append(
+        {"input": input, "output": output}
+    )
+    
 def send_message(message, role, save=True):
     # shows messages in the beginning, and save them
     with st.chat_message(role):
@@ -92,15 +103,28 @@ def send_message(message, role, save=True):
         # Note that the messages are stored in a dictionary form
         save_message(message, role)
         
+        
 # Displaying messages without saving them: display saved messages
 def paint_history():
     for message in st.session_state["messages"]:
         send_message(message["message"], message["role"], save=False)
 
+def restore_memory():
+    for history in st.session_state["chat_history"]:
+        memory.save_context({"input": history["input"]}, {"output": history["output"]})
 
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
+def load_memory(input):
+    return memory.load_memory_variables({})["chat_history"]
+
+def invoke_chain(message):
+    # invoke the chain
+    result = chain.invoke(message)
+    # save the interaction in the memory
+    save_memory(message, result.content)
+    
 
 prompt = ChatPromptTemplate.from_messages([
     ("system",
@@ -108,6 +132,8 @@ prompt = ChatPromptTemplate.from_messages([
      Answer the question using ONLY the following context. If you don't know the answer, just say you don't know. DO NOT MAKE UP anything.
      Context:{context}
      """),
+    # passing chat history to the template? =>message placeholder
+    MessagesPlaceholder(variable_name="chat_history"),
     ("human", "{question}")
 ])
 
@@ -131,6 +157,8 @@ if file:
     # if file exists, retrieve and start creating messages.
     retriever = embed_file(file)
     send_message("I am ready. Ask away", "ai", save=False)
+    # Restore memory and paint the history of previous chat
+    restore_memory()
     paint_history()
     message = st.chat_input("Ask anything about your file...")
     if message:
@@ -139,10 +167,12 @@ if file:
         chain = {
             "context": retriever | RunnableLambda(format_docs),
             # sending the question straight to the prompt
-            "question": RunnablePassthrough()
+            "chat_history": load_memory,
+            "question": RunnablePassthrough(),
         } | prompt | llm
         with st.chat_message("ai"):
-            chain.invoke(message)
+            invoke_chain(message)
 else:
     # When there is no file(like in the beginning), initialize the session with a blank list
     st.session_state["messages"] = []
+    st.session_state["chat_history"] =[]
