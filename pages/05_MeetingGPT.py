e1e86efef56afdca47f8e45a6901482ad051092f
minhosong88 <hominsong@naver.com>
2023-12-31 18:57:23 -0800

    Update MeeingGPT
    Adding Q&A

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -6,18 +6,74 @@ import glob
 import openai
 import os
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import StrOutputParser
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.storage import LocalFileStore
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 
+# Define a class for callback functions
+class ChatCallbackHandler(BaseCallbackHandler):
+    # Initialize a message variable
+    message =""
+    # When llm starts, an empty box is created
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    # When llm ends, save the created message
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+
+    # Each new token generated, the function is called
+    def on_llm_new_token(self, token, *args, **kwargs):
+        # append each token to the message variable
+        self.message += token
+        # each message appened will be shown to the message box
+        self.message_box.markdown(self.message)
+
+# Create an LLM
 llm = ChatOpenAI(
     temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ]
 )
+
+# Create a memory
+memory = ConversationSummaryBufferMemory(
+    llm=llm,
+    max_token_limit=120,
+    memory_key="chat_history",
+    return_messages=True,
+)
+
 
 # Not to create the same transcript again when there is one.
 has_transcript = os.path.exists("./.cache/podcast.txt")
 
+splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=800,
+                chunk_overlap=100,
+            )
+@st.cache_data()
+def embed_file(file_path):
+    # Set a path for file storage
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    loader = TextLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embedder = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+        embedder, cache_dir
+    )
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
 
 @st.cache_data()
 def transcribe_chunks(chunk_folder, destination):
@@ -46,6 +102,46 @@ def extract_audio_from_video(video_path):
     audio_path = video_path.replace("mp4", "mp3")
     command = ['ffmpeg', '-y', '-i', video_path, "-vn", audio_path]
     subprocess.run(command)
+
+# Save the message and memory to the session_state
+def save_message(message, role):
+    st.session_state["messages"].append(
+        {"message": message, "role": role}
+    )
+def save_memory(input, output):
+    st.session_state["chat_history"].append(
+        {"input": input, "output": output}
+    )
+    
+def send_message(message, role, save=True):
+    # shows messages in the beginning, and save them
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        # Note that the messages are stored in a dictionary form
+        save_message(message, role)
+        
+        
+# Displaying messages without saving them: display saved messages
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+def restore_memory():
+    for history in st.session_state["chat_history"]:
+        memory.save_context({"input": history["input"]}, {"output": history["output"]})
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+def load_memory(input):
+    return memory.load_memory_variables({})["chat_history"]
+
+def invoke_chain(message):
+    # invoke the chain
+    result = chain.invoke(message)
+    # save the interaction in the memory
+    save_memory(message, result.content)
 
 
 @st.cache_data()
@@ -112,10 +208,6 @@ if video:
 
         if generate:
             loader = TextLoader(transcript_path)
-            splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-                chunk_size=800,
-                chunk_overlap=100,
-            )
             docs = loader.load_and_split(text_splitter=splitter)
             
             first_summary_prompt = ChatPromptTemplate.from_template(
@@ -126,7 +218,7 @@ if video:
                 """
             )
             first_summary_chain = first_summary_prompt|llm|StrOutputParser()
-            
+            # Create an initial summary
             summary = first_summary_chain.invoke({
                 "text":docs[0].page_content
             })
@@ -145,6 +237,7 @@ if video:
             )
             
             refine_chain =  refine_prompt|llm|StrOutputParser()
+            # Iterate the documents based on the initial summary
             with st.status("Summarizing...") as status:
                 for idx, doc in enumerate(docs[1:]):
                     status.update(label=f"Processing document # {idx+1} / {len(docs[1:])}")
@@ -154,3 +247,34 @@ if video:
                         "context": doc.page_content,
                     })
             st.write(summary)
+            
+    with qa_tab:
+        retriever = embed_file(transcript_path)
+        prompt = ChatPromptTemplate.from_messages([
+        ("system",
+        """
+        Answer the question using ONLY the following context. If you don't know the answer, just say you don't know. DO NOT MAKE UP anything.
+        Context:{context}
+        """),
+        # passing chat history to the template? =>message placeholder
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}")
+        ])
+        send_message("Ask anything about the video", "ai", save=False)
+        restore_memory()
+        paint_history()
+        question = st.chat_input("Type your question here.")
+        if question:
+            send_message(question, "human")
+            chain = {
+            "context": retriever | RunnableLambda(format_docs),
+            # sending the question straight to the prompt
+            "chat_history": load_memory,
+            "question": RunnablePassthrough(),
+            } | prompt | llm
+            with st.chat_message("ai"):
+                invoke_chain(question)
+        else:
+            # When there is no file(like in the beginning), initialize the session with a blank list
+            st.session_state["messages"] = []
+            st.session_state["chat_history"] =[]
