fe2fa8d7b2107688c9f50829ed0a632ca1241f62
minhosong88 <hominsong@naver.com>
2024-01-04 20:25:16 -0800

    Update 05_MeetingGPT.py
    Adding Q&A tabs

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import subprocess
-from pydub import AudioSegment
 import math
+from pydub import AudioSegment
 import glob
 import openai
 import os
@@ -12,10 +12,11 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import StrOutputParser
 from langchain.vectorstores.faiss import FAISS
 from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
-from langchain.memory import ConversationSummaryBufferMemory
+from langchain.memory import ConversationSummaryBufferMemory, ConversationBufferMemory
 from langchain.storage import LocalFileStore
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+
 
 # Define a class for callback functions
 class ChatCallbackHandler(BaseCallbackHandler):
@@ -45,18 +46,23 @@ llm = ChatOpenAI(
     ]
 )
 
+summary_llm = ChatOpenAI(
+    temperature=0.1,
+    streaming=True,
+)
+
 # Create a memory
-memory = ConversationSummaryBufferMemory(
+memory = ConversationBufferMemory(
     llm=llm,
     max_token_limit=120,
     memory_key="chat_history",
     return_messages=True,
 )
 
-
-# Not to create the same transcript again when there is one.
+# Not to create the same transcript again when there is one already
 has_transcript = os.path.exists("./.cache/podcast.txt")
 
+# Create a splitter
 splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
                 chunk_size=800,
                 chunk_overlap=100,
@@ -74,34 +80,6 @@ def embed_file(file_path):
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
     return retriever
-
-@st.cache_data()
-def transcribe_chunks(chunk_folder, destination):
-    if has_transcript:
-        return
-    # Grab the files with glob module
-    files = glob.glob(f"{chunk_folder}/*.mp3")
-    # Sort the files in an ascending order because glob gets files in a random order
-    files.sort()
-    # Iterate files, open and transcribe into a text file
-    for file in files:
-        with open(file, "rb") as audio_file, open(destination, "a") as text_file:
-            # Transcribe the text
-            transcript = openai.Audio.transcribe(
-                "whisper-1",
-                audio_file,
-            )
-            # Write the transcript in the destination path
-            text_file.write(transcript["text"])
-
-
-@st.cache_data()
-def extract_audio_from_video(video_path):
-    if has_transcript:
-        return
-    audio_path = video_path.replace("mp4", "mp3")
-    command = ['ffmpeg', '-y', '-i', video_path, "-vn", audio_path]
-    subprocess.run(command)
 
 # Save the message and memory to the session_state
 def save_message(message, role):
@@ -144,36 +122,70 @@ def invoke_chain(message):
     save_memory(message, result.content)
 
 
+
+@st.cache_data()
+def transcribe_chunks(chunk_folder, destination):
+    if has_transcript:
+        return
+    # Grab the files with glob module
+    files = glob.glob(f"{chunk_folder}/*.mp3")
+    # Sort the files in an ascending order because glob gets files in a random order
+    files.sort()
+    # iterate files, open ans transcribe into a text file
+    for file in files:
+        with open(file, "rb") as audio_file, open(destination, "a") as text_file:
+            # Transcribe the text
+            transcript = openai.Audio.transcribe(
+                "whisper-1",
+                audio_file,
+            )
+            # Write the transcript in the destination path
+            text_file.write(transcript["text"])
+
+
+@st.cache_data()
+def extract_audio_from_video(video_path):
+    if has_transcript:
+        return
+    audio_path = video_path.replace("mp4", "mp3")
+    command = ["ffmpeg", "-y", "-i", video_path, "-vn", audio_path,]
+    subprocess.run(command)
+
+
 @st.cache_data()
 def cut_audio_in_chunks(audio_path, chunk_size, chunks_folder):
     if has_transcript:
         return
     track = AudioSegment.from_mp3(audio_path)
-    chunk_len = chunk_size*60*1000
-    chunks = math.ceil(len(track)/chunk_len)
+    chunk_len = chunk_size * 60 * 1000
+    chunks = math.ceil(len(track) / chunk_len)
     for i in range(chunks):
         start_time = i * chunk_len
-        end_time = (i+1) * chunk_len
-
+        end_time = (i + 1) * chunk_len
         chunk = track[start_time:end_time]
-        chunk.export(f"{chunks_folder}/chunk_{i}.mp3", format="mp3")
+        chunk.export(f"./{chunks_folder}/chunk_{i}.mp3", format="mp3",)
 
 
 st.set_page_config(
     page_title="MeetingGPT",
-    page_icon="🧑‍💻"
+    page_icon="💼",
 )
-st.title("MeetingGPT")
+
 st.markdown(
     """
-    Welcome to MeetingGPT, upload a video and I will give you a transcript, a summary and a chat bot to ask any questions about it.
-    
-    Get started by uploading a video file in the side bar
-    """
+# MeetingGPT
+            
+Welcome to MeetingGPT, upload a video and I will give you a transcript, a summary and a chat bot to ask any questions about it.
+
+Get started by uploading a video file in the sidebar.
+"""
 )
 
 with st.sidebar:
-    video = st.file_uploader("Video", type=["mp4", "avi", "mkv", "mov"])
+    video = st.file_uploader(
+        "Video",
+        type=["mp4", "avi", "mkv", "mov"],
+    )
 
 if video:
     chunks_folder = "./.cache/chunks"
@@ -190,11 +202,13 @@ if video:
         status.update(label="Cutting audio segments...")
         cut_audio_in_chunks(audio_path, 10, chunks_folder)
         status.update(label="Transcribing audio...")
+        status.update(label="Complete.")
         transcribe_chunks(chunks_folder, transcript_path)
 
+    # Create tabs for transcript, summary, and Q&A
     transcript_tab, summary_tab, qa_tab = st.tabs(
         [
-            "Transcrript",
+            "Transcript",
             "Summary",
             "Q&A",
         ]
@@ -203,52 +217,61 @@ if video:
     with transcript_tab:
         with open(transcript_path, "r") as file:
             st.write(file.read())
+
     with summary_tab:
         generate = st.button("Generate summary")
 
         if generate:
             loader = TextLoader(transcript_path)
+            splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=800,
+                chunk_overlap=100,
+            )
             docs = loader.load_and_split(text_splitter=splitter)
-            
+
             first_summary_prompt = ChatPromptTemplate.from_template(
                 """
                 Write a concise summary of the following:
                 "{text}"
-                CONCISE SUMMARY:
-                """
+                CONCISE SUMMARY:                
+            """
             )
-            first_summary_chain = first_summary_prompt|llm|StrOutputParser()
-            # Create an initial summary
-            summary = first_summary_chain.invoke({
-                "text":docs[0].page_content
-            })
+
+            first_summary_chain = first_summary_prompt | summary_llm | StrOutputParser()
+
+            summary = first_summary_chain.invoke(
+                {"text": docs[0].page_content},
+            )
 
             refine_prompt = ChatPromptTemplate.from_template(
                 """
                 Your job is to produce a final summary.
-                We have provided an existing summary up to a certain point:{existing_summary}
-                We have the opportunity to refine the existing summary(Only if needed) with some more context below.
-                -------------------
+                We have provided an existing summary up to a certain point: {existing_summary}
+                We have the opportunity to refine the existing summary (only if needed) with some more context below.
+                ------------
                 {context}
-                -------------------
+                ------------
                 Given the new context, refine the original summary.
-                If the context isn't useful, RETURN the original summary
+                If the context isn't useful, RETURN the original summary.
                 """
             )
-            
-            refine_chain =  refine_prompt|llm|StrOutputParser()
-            # Iterate the documents based on the initial summary
+
+            refine_chain = refine_prompt | summary_llm | StrOutputParser()
+
             with st.status("Summarizing...") as status:
-                for idx, doc in enumerate(docs[1:]):
-                    status.update(label=f"Processing document # {idx+1} / {len(docs[1:])}")
-                    # Keep updating summary based on the previous summary
-                    summary = refine_chain.invoke({
-                        "existing_summary": summary,
-                        "context": doc.page_content,
-                    })
+                for i, doc in enumerate(docs[1:]):
+                    status.update(
+                        label=f"Processing document {i+1}/{len(docs)-1} ")
+                    summary = refine_chain.invoke(
+                        {
+                            "existing_summary": summary,
+                            "context": doc.page_content,
+                        }
+                    )
             st.write(summary)
-            
+
     with qa_tab:
+        # When there is no file(like in the beginning), initialize the session with a blank list
         retriever = embed_file(transcript_path)
         prompt = ChatPromptTemplate.from_messages([
         ("system",
@@ -263,7 +286,7 @@ if video:
         send_message("Ask anything about the video", "ai", save=False)
         restore_memory()
         paint_history()
-        question = st.chat_input("Type your question here.")
+        question = st.text_input("Type your question here.")
         if question:
             send_message(question, "human")
             chain = {
@@ -274,7 +297,6 @@ if video:
             } | prompt | llm
             with st.chat_message("ai"):
                 invoke_chain(question)
-        else:
-            # When there is no file(like in the beginning), initialize the session with a blank list
-            st.session_state["messages"] = []
-            st.session_state["chat_history"] =[]
+else:
+    st.session_state["messages"] = []
+    st.session_state["chat_history"] =[] 
